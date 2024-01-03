e0ebe66a1f2010ad3cf5d65252036b85060088c2
izzy-2023 <izzyzinxhirija@gmail.com>
2024-01-02 21:59:30 -0500

    completed full crud app

--- server.js
+++ server.js
@@ -106,23 +106,80 @@ app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs")
 })
 
-
-  // Show route
-app.get("/fruits/:id", async (req, res) => {
+// create route
+app.post("/fruits", async (req, res) => {
   try {
-    // get the id from params
-    const id = req.params.id;
-
-    // find the particular fruit from the database
-    const fruit = await Fruit.findById(id);
-
-    // render the template with the data from the database
-    res.render("fruits/show.ejs", { fruit });
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // create the new fruit
+    await Fruit.create(req.body);
+    // redirect the user back to the main fruits page after fruit created
+    res.redirect("/fruits");
   } catch (error) {
-    console.log("---------", error.message, "----------")
+    console.log("-----", error.message, "------")
     res.status(400).send(error.message);
   }
 });
+
+// Edit Route (Get to /fruits/:id/edit)
+app.get("/fruits/:id/edit", async (req, res) => {
+  try {
+    // get the id from params
+    const id = req.params.id;
+    // get the fruit from the db
+    const fruit = await Fruit.findById(id);
+    //render the template
+    res.render("fruits/edit.ejs", { fruit });
+  } catch (error) {
+    console.log("-----", error.message, "------");
+    res.status(400).send("error, read logs for details");
+  }
+});
+
+//update route
+app.put("/fruits/:id", async (req, res) => {
+  try {
+    // get the id from params
+    const id = req.params.id;
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // update the fruit
+    await Fruit.findByIdAndUpdate(id, req.body, { new: true });
+    // redirect user back to main page when fruit
+    res.redirect(`/fruits/${id}`);
+  } catch (error) {
+    console.log("-----", error.message, "------");
+    res.status(400).send(error.message);
+  }
+});
+
+// The Delete Route (delete to /fruits/:id)
+app.delete("/fruits/:id", async (req, res) => {
+  // get the id
+  const id = req.params.id
+  // delete the fruit
+  await Fruit.findByIdAndDelete(id)
+  // redirect to main page
+  res.redirect("/fruits")
+})
+
+
+// The Show Route (Get to /fruits/:id)
+app.get("/fruits/:id", async (req, res) => {
+  try{
+      // get the id from params
+      const id = req.params.id
+
+      // find the particular fruit from the database
+      const fruit = await Fruit.findById(id)
+
+      // render the template with the fruit
+      res.render("fruits/show.ejs", {fruit})
+  }catch(error){
+      console.log("-----", error.message, "------")
+      res.status(400).send("error, read logs for details")
+  }
+})
 
   
   
